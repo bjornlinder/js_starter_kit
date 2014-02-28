9246c759842324eac98f7160b5ee637cc4b91e26
Bjorn Linder <bjornishungry@hotmail.com>
2014-02-28 12:04:02 -0500

    guess the number assignment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 alert('Hey find me to know where to put the javascript');
+random=Math.floor(Math.random()*9);
+var name=prompt("Hey there baby, what's your name?");
+var number=prompt("Yo girl, gimme your number. Cause uh, I lost mine.");
+if (number === 9) {
+alert("Awesome! I'll call you sometime, k?");
+} else {
+	alert("That's the number for a suicide help hotline, isn't it?")
+}
